8354824b83180f311f6a2148935f11d38e823c98
zhandalin <zhandalin@miaohi.com>
2018-12-03 17:11:01 +0800

    build文件处理

--- app/src/main/java/com/luoye/bzffmpeg/MainActivity.java
+++ app/src/main/java/com/luoye/bzffmpeg/MainActivity.java
@@ -1,9 +1,16 @@
 package com.luoye.bzffmpeg;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.luoye.bzmedia.FFmpegUtil;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("bzffmpegcmd");
     }
 
+    private static final String TAG = "bz_MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +29,39 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
+        requestPermission();
+    }
+
+    private boolean requestPermission() {
+        ArrayList<String> permissionList = new ArrayList<>();
+        //内存卡权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN && !PermissionUtil.isPermissionGranted(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            permissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+        if (!PermissionUtil.isPermissionGranted(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!PermissionUtil.isPermissionGranted(this, Manifest.permission.CAMERA)) {
+            permissionList.add(Manifest.permission.CAMERA);
+        }
+        if (!PermissionUtil.isPermissionGranted(this, Manifest.permission.RECORD_AUDIO)) {
+            permissionList.add(Manifest.permission.RECORD_AUDIO);
+        }
+
+        String[] permissionStrings = new String[permissionList.size()];
+        permissionList.toArray(permissionStrings);
+
+        if (permissionList.size() > 0) {
+            PermissionUtil.requestPermission(this, permissionStrings, PermissionUtil.CODE_REQ_PERMISSION);
+            return false;
+        } else {
+            Log.d(TAG, "所要的权限全都有了");
+            return true;
+        }
     }
 
     public void start(View view) {
-        String cmd = "ffmpeg -y -i /sdcard/Filter/input.mp4 -vcodec copy -acodec copy /sdcard/Filter/output.mp4";
+        String cmd = "ffmpeg -y -i /sdcard/bzmedia/temp_132.mp4 -i /sdcard/bzmedia/2岁男童差一秒被甩下电梯 地铁站务员飞奔救人-高清视频在线观看-资讯-爱奇艺.mp4 -map 0:v -vcodec copy -map 1:a -acodec copy /storage/emulated/0/bzmedia/video_replace_1543826101491872.mp4";
 
 //        String mergeCmd = "ffmpeg -y -i %s -i %s -c:v copy -filter_complex [0:a]aformat=fltp:44100:stereo,volume=%.2f,apad[0a];[1]aformat=fltp:44100:stereo,volume=%.2f[1a];[0a][1a]amerge[a] -map 0:v -map [a] -ac 2 %s";
 //        mergeCmd = String.format(mergeCmd, "/sdcard/Filter/temp_6.mp4", "/sdcard/Filter/input_bg.mp3", 0.5f, 1.0f, "/sdcard/Filter/output.mp4");
@@ -31,8 +69,8 @@ public class MainActivity extends AppCompatActivity {
 
 //        cmd = "ffmpeg -y -i /sdcard/Filter/temp_6.mp4 -i /sdcard/Filter/input_bg.aac -filter_complex [0:a]aformat=fltp:44100:stereo,volume=0.5,apad[0a];[1]aformat=fltp:44100:stereo,volume=1[1a];[0a][1a]amerge[a] -map 0:v -map [a] -ac 2 /sdcard/Filter/output.mp4";
         long startTime = System.currentTimeMillis();
-//        int ret = FFmpegUtil.executeFFmpegCommand(cmd, true);
-//        Log.d("bookzhan", "ret=" + ret + "-----耗时=" + (System.currentTimeMillis() - startTime));
+        int ret = FFmpegUtil.executeFFmpegCommand(cmd, true);
+        Log.d(TAG, "ret=" + ret + "-----耗时=" + (System.currentTimeMillis() - startTime));
     }
 
 }
