a7142df61f1d8aa3f7c6941fab347aeed4f5086b
zhandalin <zhandalin@miaohi.com>
2019-02-25 14:32:12 +0800

    执行加锁验证成功

--- app/src/main/cpp/cmdutilt/ffmpeg_cmd.c
+++ app/src/main/cpp/cmdutilt/ffmpeg_cmd.c
@@ -1,8 +1,22 @@
 #include <string.h>
 #include "ffmpeg.h"
 
+//保证同时只能一个线程执行
+static pthread_mutex_t cmdLock;
+static int cmdLockHasInit = 0;
+
 int executeFFmpegCommand(int64_t handle, const char *command,
                          void (*progressCallBack)(int64_t, int, float)) {
+    if (NULL == command) {
+        av_log(NULL, AV_LOG_ERROR, "NULL==command");
+        return -1;
+    }
+    av_log(NULL, AV_LOG_DEBUG, "cmd=%s", command);
+    if (!cmdLockHasInit) {
+        pthread_mutex_init(&cmdLock, NULL);//初始化
+        cmdLockHasInit = 1;
+    }
+    pthread_mutex_lock(&cmdLock);
 
     char *pCommand = (char *) command;
     int stingLen = strlen(command);
@@ -35,15 +49,16 @@ int executeFFmpegCommand(int64_t handle, const char *command,
             buffer = NULL;
         }
     }
-    for (int i = 0; i < index; ++i) {
-        av_log(NULL, AV_LOG_DEBUG, "cmd=%s", argv[i]);
-    }
+//    for (int i = 0; i < index; ++i) {
+//        av_log(NULL, AV_LOG_DEBUG, "cmd=%s", argv[i]);
+//    }
     //手动告诉它结束了,防止出现意外
     argv[index] = 0;
     int ret = exe_ffmpeg_cmd(handle, index, argv, progressCallBack);
     for (int i = 0; i < index; ++i) {
         free(argv[i]);
     }
+    pthread_mutex_unlock(&cmdLock);
     return ret;
 }
 
